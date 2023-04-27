9ffdd2079b2f6f255f0ae25aa6d0c4d92c6bec19
Andrew-debug <yatsenkoandy97@gmail.com>
2023-04-27 22:45:50 +0300

    section base

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -29,3 +34,48 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// cookie element
+message.classList.add('cookie-message');
+message.innerHTML =
+  '<p>We use cookies for improved functionality and analytics.</p> <button class="btn btn--close-cookie">Got it!</button>';
+header.append(message);
+
+// сlose cookie message
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.remove());
+
+// smooth scrolling via links
+// event delegation
+document.querySelector('.nav__links').addEventListener('click', e => {
+  e.preventDefault();
+  // matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+btnScrollTo.addEventListener('click', () => {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Tabbed component
+tabsContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  // remove active class
+  tabs.forEach(el => el.classList.remove('operations__tab--active'));
+  tabsContent.forEach(el => el.classList.remove('operations__content--active'));
+  // activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// Menu fade animation
